1738d1eccf3782a5d97548d40c9476da767d52ef
birtezuidinga <zuidinga@neuro.mpg.de>
2021-10-06 18:42:27 +0200

    Symmetrical with respect to midline.
    (cherry picked from commit 10ca3383e5d307521d8c445e365bec0c947c57c2)

--- stytra/birte_experiments/bilateral_moving_gratings.py
+++ stytra/birte_experiments/bilateral_moving_gratings.py
@@ -18,11 +18,10 @@ class CombinedProtocol(Protocol):
         vel = np.array([0, 0, 10, 10, 0, 0])
 
         df = pd.DataFrame(dict(t=t, vel_x=vel))
+        s_a = MovingGratingStimulus2(df_param=df, grating_angle=np.pi, grating_period=31.68, wave_shape="sine", clip_mask=[0, 0, 0.5, 1])
 
-        s_a = MovingGratingStimulus2(df_param=df, grating_angle=np.pi, grating_period=20, wave_shape="sine", clip_mask=[0, 0, 0.5, 1])
-
-        df = pd.DataFrame(dict(t=t, vel_x=-vel))
-        s_b = MovingGratingStimulus2(df_param=df, grating_angle=np.pi, wave_shape="sine", clip_mask=[0.5, 0, 0.5, 1])
+        df = pd.DataFrame(dict(t=t, vel_x=vel))
+        s_b = MovingGratingStimulus2(df_param=df, grating_angle=0, grating_period=31.68, wave_shape="sine", clip_mask=[0.5, 0, 0.5, 1])
 
         stimuli = [CombinerStimulus([s_a, s_b])]
         return stimuli
