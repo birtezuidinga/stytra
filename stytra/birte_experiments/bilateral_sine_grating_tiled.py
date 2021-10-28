f1edc43816f8729071a98e61be59323ae1593965
Birte Zuidinga <zuidinga@neuro.mpg.de>
2021-10-28 13:51:36 +0200

    Parametrized wave_period
    (cherry picked from commit f843c607d61083c7dc8ea4f0233e4c10f4b16469)

--- stytra/birte_experiments/bilateral_sine_grating_tiled.py
+++ stytra/birte_experiments/bilateral_sine_grating_tiled.py
@@ -7,10 +7,11 @@ import qimage2ndarray
 from lightparam import Param
 
 
-def create_pattern(side_len=1000, wave_shape="sine", color_1=(255, 255, 255), color_2=(0, 0, 0), path="generated_pattern.png"):
+def create_pattern(side_len=1000, wave_shape="sine", wave_period=30, color_1=(255, 255, 255), color_2=(0, 0, 0), path="generated_pattern.png"):
     x = (np.arange(side_len) - side_len / 2) / side_len
     xx, yy = np.meshgrid(x, x)  # grid of points
-    W = np.sin(xx * 4 * 2 * np.pi)
+    factor = 180 / wave_period
+    W = np.sin(xx * factor * 2 * np.pi)
     W = ((W + 1) / 2)[:, :, np.newaxis]  # normalize and add color axis
 
     if wave_shape == "square":
@@ -28,34 +29,38 @@ class CombinedMovingTiledGrating(Protocol):
 
     def __init__(self):
         super().__init__()
-        self.display_width_px = Param(960, limits=(10, 5000))
+        self.display_width_px = Param(1920, limits=(10, 5000))
         self.wave_shape = Param("sine", limits=["sine", "square"])
+        self.wave_period = Param(30, limits=(1, 1000))  # in degrees
         self.contrast = Param(100, limits=(0, 255))
         self.velocity = Param(20, limits=(-1000, 1000))
+        self.duration = Param(20, limits=(1., 1000))
 
     def get_stim_sequence(self):
         display_width_px = self.display_width_px
         shape = self.wave_shape
+        period = self.wave_period
         contrast = self.contrast
         vel = self.velocity
+        duration = self.duration
 
         c1 = 127.5 + contrast / 2
         c2 = 127.5 - contrast / 2
 
         Stim = type("stim", (SeamlessImageStimulus, InterpolatedStimulus), {})
 
-        create_pattern(side_len=display_width_px, wave_shape=shape, color_1=(c1, c1, c1), color_2=(c2, c2, c2),
+        create_pattern(side_len=display_width_px, wave_shape=shape, wave_period=period, color_1=(c1, c1, c1), color_2=(c2, c2, c2),
                        path="assets/generated_pattern.png")
 
         s_a = Stim(
                 background="generated_pattern.png",
-                df_param=pd.DataFrame(dict(t=[0, 30], vel_x=[vel, vel], vel_y=[0, 0])),
+                df_param=pd.DataFrame(dict(t=[0, duration], vel_x=[vel, vel], vel_y=[0, 0])),
                 clip_mask=[0, 0, 0.5, 1],
                 rotation=np.pi)
 
         s_b = Stim(
                 background="generated_pattern.png",
-                df_param=pd.DataFrame(dict(t=[0, 30], vel_x=[vel, vel], vel_y=[0, 0])),
+                df_param=pd.DataFrame(dict(t=[0, duration], vel_x=[vel, vel], vel_y=[0, 0])),
                 clip_mask=[0.5, 0, 0.5, 1],
                 rotation=0)
 
