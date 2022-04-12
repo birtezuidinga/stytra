b9955a00936d271f6487536934650985e8ab0419
birtezuidinga <zuidinga@neuro.mpg.de>
2022-04-12 22:08:13 +0200

    Multiple looming stimulus first working version.

--- stytra/stimulation/stimuli/visual.py
+++ stytra/stimulation/stimuli/visual.py
@@ -4,6 +4,7 @@ import numpy as np
 import pims
 import qimage2ndarray
 from pathlib import Path
+import math
 
 from PyQt5.QtCore import QPoint, QRect, QPointF, Qt
 from PyQt5.QtGui import QPainter, QBrush, QColor, QPen, QTransform, QPolygon, QRegion
@@ -962,7 +963,7 @@ class CurvedScreenCircleStimulus(VisualStimulus, DynamicStimulus):
     radius : float
         circle radius (in deg for curved_screen == True, in mm for curved_screen == False)
 
-    backgroud_color : tuple(int, int, int)
+    background_color : tuple(int, int, int)
         RGB color of the background
 
     circle_color : tuple(int, int, int)
@@ -1020,6 +1021,140 @@ class CurvedScreenCircleStimulus(VisualStimulus, DynamicStimulus):
         p.setBrush(QBrush(QColor(*self.circle_color)))
         p.drawEllipse(QPointF(x, y),
                       self.radius / mm_px, self.radius / mm_px)
+
+
+class CurvedScreenMultipleCirclesStimulus(VisualStimulus, DynamicStimulus):
+    """ A filled circle stimulus, which in combination with interpolation
+    can be used to make looming stimuli, multiple circles with different luminance
+
+    Parameters
+    ---------
+    origin : tuple(float, float)
+        positions of the circle centre (as fraction of screen size)
+
+    radius : float
+        circle radius (in deg for curved_screen == True, in mm for curved_screen == False)
+
+    background_color : tuple(int, int, int)
+        RGB color of the background
+
+    circle_color : tuple(int, int, int)
+        RGB color of the circle
+
+    curved_screen : bool
+        when using a curved screen (as half a cylinder, 180 degrees horizontally), setting this to True enables you to
+        provide the radius of the circle in degrees of visual space instead of mm. It will be calculated to pixels
+        based on the display window settings.
+
+    """
+
+    def __init__(
+        self,
+        *args,
+        origin=(0.5, 0.5),
+        radius=10,
+        background_color=(0, 0, 0),
+        circle_color=(255, 255, 255),
+        curved_screen=False,
+        max_loom_diameter=180,
+        inner_circles_velocity="constant",
+        constant_circles_interval=10,
+        faster_circles_start_interval=5,
+        faster_circles_end_interval=5,
+        slower_circles_slowing_rate=0.5,
+        slower_circles_n=200,
+        **kwargs
+    ):
+        super().__init__(*args, dynamic_parameters=["x", "y", "radius"], **kwargs)
+        self.x = origin[0]
+        self.y = origin[1]
+        self.radius = radius
+        self.background_color = background_color
+        self.circle_color = circle_color
+        self.name = "circle"
+        self.curved_screen = curved_screen
+        self.max_loom_radius = max_loom_diameter / 2
+        self.inner_circles_velocity = inner_circles_velocity
+        self.constant_circles_interval = constant_circles_interval
+        self.faster_circles_start_interval = faster_circles_start_interval
+        self.faster_circles_end_interval = faster_circles_end_interval
+        self.slower_circles_slowing_rate = slower_circles_slowing_rate
+        self.slower_circles_n = slower_circles_n
+
+    def paint(self, p, w, h):
+        super().paint(p, w, h)
+
+        if self._experiment.calibrator is not None:
+            mm_px = self._experiment.calibrator.mm_px
+        else:
+            mm_px = 1
+
+        # for a curved screen, the radius of the circle can be defined as degrees of visual space in the protocol,
+        # here it is converted back to mm, based on the calibration of the screen
+        if self.curved_screen:
+            self.radius = self.radius / 180 * (w * mm_px)
+            max_loom_radius = self.max_loom_radius / 180 * (w * mm_px)
+
+        x = self.x * w
+        y = self.y * h
+
+        # draw the background
+        p.setPen(Qt.NoPen)
+        p.setBrush(QBrush(QColor(*self.background_color)))
+        self.clip(p, w, h)
+        p.drawRect(QRect(-1, -1, w + 2, h + 2))
+
+        if self.inner_circles_velocity == "constant":
+
+            distances = self.constant_circles_interval
+            n_circles = math.floor(self.radius / distances) + 1
+
+            for i in range(n_circles - 1):
+                # draw the circle
+                i_circle_color = self.circle_color[1] + i * 10
+                # CHANGE contrasts
+
+                p.setBrush(QBrush(QColor(i_circle_color, i_circle_color, i_circle_color, 255)))
+                p.drawEllipse(QPointF(x, y),
+                              (self.radius - distances * i) / mm_px, (self.radius - distances * i) / mm_px)
+
+        elif self.inner_circles_velocity == "faster":
+            starting_intervals = self.faster_circles_start_interval
+            ending_intervals = self.faster_circles_end_interval
+            n_circles = math.ceil(max_loom_radius / ending_intervals)
+
+            # draw the circles
+            for i in range(n_circles - 1):
+
+                if i % 2 == 0:
+                    i_circle_color = self.circle_color[1]
+                else:
+                    i_circle_color = self.background_color[1]
+
+                i_circle_radius = self.radius - (i * starting_intervals * (max_loom_radius - self.radius)/max_loom_radius + i * ending_intervals)
+
+                if i_circle_radius > 0:
+                    p.setBrush(QBrush(QColor(i_circle_color, i_circle_color, i_circle_color, 255)))
+                    p.drawEllipse(QPointF(x, y),
+                                  i_circle_radius / mm_px, i_circle_radius / mm_px)
+
+        else:
+            n_circles = self.slower_circles_n
+
+            # draw the circles
+            for i in range(n_circles - 1):
+
+                if i % 2 == 0:
+                    i_circle_color = self.circle_color[1]
+                else:
+                    i_circle_color = self.background_color[1]
+
+                i_circle_radius = self.radius * self.slower_circles_slowing_rate ** (i * self.radius / max_loom_radius)
+
+                if i_circle_radius > 0:
+                    p.setBrush(QBrush(QColor(i_circle_color, i_circle_color, i_circle_color, 255)))
+                    p.drawEllipse(QPointF(x, y),
+                                  i_circle_radius / mm_px, i_circle_radius / mm_px)
 
 
 class FixationCrossStimulus(FullFieldVisualStimulus):
