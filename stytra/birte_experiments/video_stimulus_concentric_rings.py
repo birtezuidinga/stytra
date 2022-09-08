53114f8a84402003b743ed82d634a47eff5edef1
Birte Zuidinga <zuidinga@neuro.mpg.de>
2022-09-08 15:15:42 +0200

    Slightly new batch of stimuli to use.

--- stytra/birte_experiments/video_stimulus_concentric_rings.py
+++ stytra/birte_experiments/video_stimulus_concentric_rings.py
@@ -15,7 +15,7 @@ class VideoProtocol(Protocol):
         super().__init__()
 
         # List all stimuli
-        json_paths = r"C:/Users/zuidinga/PycharmProjects/stytra_birte/stytra/stytra/birte_experiments/assets/stim_videos/20220812_concentric_rings/"
+        json_paths = r"C:/Users/zuidinga/PycharmProjects/stytra_birte/stytra/stytra/birte_experiments/assets/stim_videos/20220908_concentric_rings/"
         json_files = [file for file in os.listdir(json_paths) if file.endswith('.json')]
         dfs = []  # an empty list to store the data frames
         for file in json_files:
@@ -50,12 +50,12 @@ class VideoProtocol(Protocol):
         stimulus = self.stimulus
         stimulus_index = self.unique_stimuli_list.index(stimulus)
         video_name = self.unique_stimuli.loc[stimulus_index, 'name']
-        path = 'stim_videos/20220812_concentric_rings/' + video_name + '.mp4'
+        path = 'stim_videos/20220908_concentric_rings/' + video_name + '.mp4'
 
         index = self.unique_stimuli_list.index(stimulus)
         name_stimulus = self.unique_stimuli.loc[index, 'name']  # used in queries
         framerate = float(self.metadata_stimuli.query("name == @name_stimulus").fps)
-        video_duration =  float(self.metadata_stimuli.query("name == @name_stimulus").video_duration)
+        video_duration = float(self.metadata_stimuli.query("name == @name_stimulus").video_duration)
         resolution_reduction_factor = float(self.metadata_stimuli.query("name == @name_stimulus").resolution_decreasing_factor)
 
         stimuli.append(VideoStimulus(video_path=path, framerate=framerate, duration=video_duration, resolution_reduction_factor=resolution_reduction_factor))
